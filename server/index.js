f2a4ebb1532706e268774b4d3644cf0881d59bc5
rahel samson <rahelsamson953@gmail.com>
2024-05-21 06:39:14 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const db= require('./models')
+app.use(express.json());
+//routers
+const postRouter= require('./routes/Posts')
+app.use("/posts",postRouter)
+db.sequelize.sync().then(()=>{
+  
+    app.listen(3001,()=>{
+    
+        console.log("server running on port 3001")
+    });
+})
